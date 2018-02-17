e940628242f0b31369bbcb64b68d84d2b16b6a81
denisdenk <denisdenk@live.ru>
2018-02-17 15:38:22 +0300

    Removed unnecessary stuff

--- Humid_Temp.ino
+++ Humid_Temp.ino
@@ -9,17 +9,9 @@ LiquidCrystal_I2C lcd(0x3F,16,2);
 void setup()
 {
   Serial.begin(9600);
+  
   lcd.init();  
   lcd.backlight();
-  lcd.setCursor(3,0);
-  lcd.print("Hello, world!");
-  lcd.setCursor(2,1);
-  lcd.print("Ywrobot Arduino!");
-  lcd.setCursor(0,2);
-  lcd.print("Arduino LCM IIC 2004");
-  lcd.setCursor(2,3);
-  lcd.print("Power By Ec-yuan!");
-  lcd.clear();
   
   myHumidity.begin();
 }
